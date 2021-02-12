298e4f032b7875684daede4c53a97b6c26e8555b
Audun Midttun Systad <audun@godal.com>
2021-02-12 16:42:41 +0100

    initialising critic

--- critic.py
+++ critic.py
@@ -1,8 +1,22 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from random import *
 
-model = nn.sequential( 
-    nn.linear
 
-)
+class TableCritic():
+    
+    def __init__(learning_rate, discount_factor, trace_decay_factor):
+        return
+        
+    def train(starting_state):
+        values = {starting_board: random.random()*0.1}
+        moves = starting_state.get_available_moves()
+        policy = {}
+        for move in moves:
+            policy[(move, starting_state)] = 0
+
+
+        
+        
+
