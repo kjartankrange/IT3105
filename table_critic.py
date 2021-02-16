0de5163de60c73ba794d05d2a8449889a1e5f15d
Kjartan <kjartankrange@outlook.com>
2021-02-16 12:08:45 +0100

    Remove critic and add logic for table_critic class

---
+++ table_critic.py
@@ -0,0 +1,31 @@
+class Table_critic():
+
+    def __init__(self):
+        self.eligibilities = {}
+        self.values = {}
+    
+    def reset_eligibilities(self):
+        self.eligibilities = {}
+    
+    def get_eligibilities(self,state):
+        if state not in self.eligibilities.keys():
+            self.eligibilities[state] = random.random()*0.001
+        return self.eligibilities[state]
+            
+    def decay_refresh_eligibilities(self,state,gamma,l):
+        self.eligibilities[state] = 1
+        for key in self.eligibilities.keys():
+            if key != state:
+                self.eligibilities[key] = self.eligibilities[key]*gamma*l
+
+    def get_values(self,state):
+        if state not in self.values.keys():
+            self.values[state] = 0
+        return self.values[state]
+
+    def set_values(self,state, value):
+        if state not in self.values.keys():
+            self.values[state] = 0
+        self.values[state] = value
+
+ 
